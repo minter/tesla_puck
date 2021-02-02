faadbcaed42d83912175191ef900443a894e3fc1
H. Wade Minter <minter@lunenburg.org>
2021-02-01 23:22:40 -0500

    Fix scheduler error

--- lib/tesla_puck/scheduler.rb
+++ lib/tesla_puck/scheduler.rb
@@ -8,8 +8,11 @@ module TeslaPuck
       today = @time_zone.to_local(Time.now).strftime('%Y-%m-%d')
 
       @game = HTTParty.get("https://statsapi.web.nhl.com/api/v1/schedule?teamId=#{ENV['NHL_TEAM_ID']}&date=#{today}")['dates'].first
-      @scheduled = @game.nil? ? false : true
-      @scheduled = false if @game['games'].first['status']['detailedState'] == 'Postponed'
+      @scheduled = if @game.nil?
+                     false
+                   else
+                     (@game['games'].first['status']['detailedState'] != 'Postponed')
+                   end
       return if @scheduled == false
 
       @away = @game['games'].first['teams']['away']
